6e04b7e92ac7fdf1e87444295c8b60d9ac58896b
Cindy Mae Ngoho <cindymaen17@gmail.com>
2025-04-15 16:13:37 +0900

    added criteria

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,7 @@ Rails.application.routes.draw do
   namespace :api, defaults: { format: :json } do
     namespace :v1 do
       resources :cafes, only: [:index, :create]
+      resources :criteria, only: [:index]
     end
   end
   # as a user I search for a cafe
